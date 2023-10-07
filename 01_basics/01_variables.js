4569573a6337c821a87c4384d48050d41a6b30fd
Shibsankar Pramanik <102346989+Shibdustu@users.noreply.github.com>
2023-10-07 12:11:13 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "shibdustu@gmail.com"
+var accountPassword = "12345"
+// variable can be declare without any let for the javacript advancedness but this is not a good practice
+ accountCity = "Kolkata"
+let accountstate      //value can be undefinded as an output
+
+//accountId = 2
+accountEmail = "shib@gamil.com"
+accountPassword = 34243
+accountCity = "Bangaluru"
+
+console.log(accountId);
+/*
+Prefer not to use var 
+because of issue in block scrope and fucntional scope
+*/
+console.table([accountEmail,accountPassword,accountCity,accountstate])
+
