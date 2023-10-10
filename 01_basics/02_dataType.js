64519bf9c8b719b2983af49329f0abfd7efaf3be
Shibsankar Pramanik <102346989+Shibdustu@users.noreply.github.com>
2023-10-10 10:29:32 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) // we are using nodejs,not browser
+console.log(3+3) 
+console.log("Hitesh")
+
+let name = "Hitesh" //string data Type
+let age = 18 // int data type
+let isLoggedIn = false // boolean data type
+
+// number -> 2^53
+// bigint -> bohot bada number store karne k liye use hota hai
+// string ->""
+// boolean -> true/ false
+// null -> standalone value
+// undefined -> value defined nehi hua
+// symbol -> unique 
+
+//object
+
+console.log(typeof null); // object type ayega output
+console.log(typeof undefined)  // undefiined e ayega output
+
+
