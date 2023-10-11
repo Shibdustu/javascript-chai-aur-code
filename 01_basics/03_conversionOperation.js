a84bba1e58cdeeedf2218bae40b5b63e869f72f7
Shibsankar Pramanik <102346989+Shibdustu@users.noreply.github.com>
2023-10-11 12:54:54 +0000

    converstion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let  score = false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber = Number(score);
+
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+// "33" => 33 convert in number
+ // "33abc" -> NaN
+ // true => convert 1 false -> 0
+
+ let isLoggedIn = "shib"
+let booleanisLogged = Boolean(isLoggedIn)
+console.log(booleanisLogged);
+ //1 => true & 0 => flase
+ // "hitesh" => true for sting to boolean
+ // "" -> false for empty string to boolean
