64a6d5066b7d0535b0c441be01cff807a1a7d131
Shibsankar Pramanik <102346989+Shibdustu@users.noreply.github.com>
2023-10-12 10:16:07 +0000

    datatype summer

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,25 @@
+// premetive datatype
+
+// It's hava 7 catagory : String,Number,Boolean,
+//null,undefined,Symbol,BigInt
+
+// JavaScript is a dynamically typed language bcz does not require 
+// the explicit declaration of the variables before they're used
+
+//ReferenceType or Non premetive
+// Array,Object,Functions
+
+const heros = ["Mahadev","Ram","Krishna","Laxmi","Swaraswati"]; // this is array
+console.log(heros);
+// Object {} ke andar hota hair 
+
+/*   variable k aandar vi store kar sakte hai*/
+ let myObj = {
+    name : "Shibsankar",
+    age :21,
+}
+console.log(myObj);
+const myfunction = function(){
+    console.log("Hello World");
+
+}
