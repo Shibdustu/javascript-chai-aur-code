2d16cd246cfa9c12bb4e0294501027621fda1a61
Shibsankar Pramanik <102346989+Shibdustu@users.noreply.github.com>
2023-10-12 09:40:10 +0000

    comparision on javaScript

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+// console.log(2>1); // output will be boolean value true or false
+console.log("2">1);
+console.log(null>0); // output is false
+console.log(null == 0); //output is flase
+
+// undefined always false when is comapere with other
+console.log(undefined >0);
+console.log(undefined <0);
+
+// === this can cheak strictly 
+console.log("2" === 2);
