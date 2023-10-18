e75078b22ad58bf2ac72710262263f7260657820
Shibsankar Pramanik <102346989+Shibdustu@users.noreply.github.com>
2023-10-18 19:43:10 +0000

    stirng

---
+++ 01_basics/05_string.js
@@ -0,0 +1,16 @@
+const name = "shibsankar"
+const repocount = 50
+
+console.log(name + repocount ); // this is a outdated syntax
+
+// to concatinate we use bactis in modern days
+console.log(`hello my name is ${name} ans my repo count is ${repocount}`);
+
+const gameName = new String('shibsankar')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2)); // kis position pe konsa character hai check karne k liye
+console.log(gameName.indexOf('b')); // character  kis position pe  hai check karne k liye
+
+
